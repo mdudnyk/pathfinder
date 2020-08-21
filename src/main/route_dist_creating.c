2d371f87ed877d3947cc732f1bbfbc4bc224d23e
Myroslav Dudnyk <63348126+mdudnyk@users.noreply.github.com>
2020-08-21 18:45:46 +0300

    Add files via upload

--- src/main/route_dist_creating.c
+++ src/main/route_dist_creating.c
@@ -21,7 +21,8 @@ static bool fill_route_dist_arr(int start_island, int finish_island) {
         if (in_data.path_matrix[i][finish_island] == in_data.adj_matrix[in_data.num_of_islands][finish_island] 
             && in_data.visit_matrix[i][finish_island] == 0) {
             in_data.visit_matrix[i][finish_island] = 1;
-            in_data.route_arr[0] = i;
+            in_data.route_arr[0] = finish_island;
+            in_data.route_arr[1] = i;
             in_data.dist_arr[0] = in_data.adj_matrix[i][finish_island];
             if (in_data.begin_index != i && i != 0) {
                 matrix_cleaner(1 , i, temp_i, finish_island); 
@@ -32,8 +33,9 @@ static bool fill_route_dist_arr(int start_island, int finish_island) {
                 i = find_smallest_unvisited(i);
                 in_data.visit_matrix[i][temp_i] = 1;
                 matrix_cleaner(2 , i, temp_i, finish_island); 
-                in_data.route_arr[++j] = i;
-                in_data.dist_arr[j] = in_data.adj_matrix[temp_i][i];
+                in_data.dist_arr[++j] = in_data.adj_matrix[temp_i][i];
+                in_data.route_arr[j + 1] = i;
+                
             }
             return true;
         }
@@ -44,15 +46,16 @@ static bool fill_route_dist_arr(int start_island, int finish_island) {
 void route_dist_creating(int start_island) {
     int finish_island = start_island + 1;
     int temp_i = 0;
-
     in_data.cleaner_flag[0] = 0;
+
     for (int i = start_island + 1; i < in_data.num_of_islands; i++) {
+        route_dist_holder_malloc();
         in_data.begin_index = 0;
         init_help_arr(1);
         while(1) {
             init_help_arr(2);
             if (fill_route_dist_arr(start_island, finish_island)) {
-                print_result(start_island, finish_island);
+                route_dist_holder_arr_adder();
                 if (in_data.cleaner_flag[0] == 1) {
                     in_data.cleaner_flag[0] = 0;
                     matrix_cleaner(3 , i, temp_i, finish_island); 
@@ -61,6 +64,9 @@ void route_dist_creating(int start_island) {
             else
                 break;
         }
+        sort_results();
+        print_results(start_island, finish_island);
+        route_dist_holder_mem_free();
         finish_island++;
     }
 }
